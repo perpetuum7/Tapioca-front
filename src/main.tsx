c86b2d771eb4b45ec2978990839dfb5c1ec6b4df
thiagokroger <thiagokroger@gmail.com>
2022-03-07 16:00:06 -0300

    feat: config i18n

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "@/components/App";
 import "@/styles/index.scss";
+import "@/config/i18n";
 
 ReactDOM.render(
   <React.StrictMode>
