90216f732ed5b8162d53b967f89f5c4c84fa8eb1
thiagokroger <thiagokroger@gmail.com>
2022-03-09 15:32:18 -0300

    feat: create button component and gruup button updates

--- src/components/GroupButton.tsx
+++ src/components/GroupButton.tsx
@@ -3,32 +3,41 @@ interface Props {
   options: { id: string; children: string | JSX.Element }[];
   size?: string;
   selectItem?: (id: string) => void;
+  noBackground?: boolean;
 }
-const GroupButton = ({ selectedOption, options, size, selectItem }: Props) => {
-  const buttonClassName = ["button-group"].join(" ");
+const GroupButton = ({
+  selectedOption,
+  options,
+  size,
+  selectItem,
+  noBackground = false,
+}: Props) => (
+  <div
+    className={`button-group ${
+      noBackground ? "button-group--transparent" : ""
+    }`}
+  >
+    {options.map(({ id, children }) => {
+      const className = [
+        "button-group--item",
+        selectedOption === id ? "button-group--item-selected" : "",
+        size ? `button-group--item-${size}` : "",
+        selectItem ? "button-group--item-selectable" : "",
+        noBackground ? "button-group--transparent" : "",
+      ].join(" ");
 
-  return (
-    <div className={buttonClassName}>
-      {options.map(({ id, children }) => {
-        const className = [
-          "button-group--item",
-          selectedOption === id ? "button-group--item-selected" : "",
-          size ? `button-group--item-${size}` : "",
-          selectItem ? "button-group--item-selectable" : "",
-        ].join(" ");
-        return (
-          <button
-            onClick={() => {
-              if (selectItem) selectItem(id);
-            }}
-            className={className}
-          >
-            {children}
-          </button>
-        );
-      })}
-    </div>
-  );
-};
+      return (
+        <button
+          onClick={() => {
+            if (selectItem) selectItem(id);
+          }}
+          className={className}
+        >
+          {children}
+        </button>
+      );
+    })}
+  </div>
+);
 
 export default GroupButton;
