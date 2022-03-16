9b09b8aef3e036f1db5acfe2d8d5e6d4ee5706ad
thiagokroger <thiagokroger@gmail.com>
2022-03-16 15:05:12 -0300

    fix: update redirects for netlify

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -12,7 +12,7 @@ const App = () => {
 
           <Routes>
             <Route path="/" element={<Outlet />}>
-              <Route path="borrow" element={<Borrow />} />
+              <Route index element={<Borrow />} />
             </Route>
           </Routes>
         </div>
